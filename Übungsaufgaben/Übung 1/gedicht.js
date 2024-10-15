92eb358e87b794d04552029801540158ecc689a9
jennisinger <jsi47883@stud.hs-furtwangen.de>
2024-10-15 11:40:39 +0200

    verbessert

--- Übungsaufgaben/Übung 1/gedicht.js
+++ Übungsaufgaben/Übung 1/gedicht.js
@@ -1,2 +1,27 @@
-"use strict";
-bla;
+var RandomPoem;
+(function (RandomPoem) {
+    // Arrays für Subjekte, Prädikate und Objekte
+    var subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    var predicates = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    var objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    // Funktion zur Generierung eines Verses
+    function getVerse(_subjects, _predicates, _objects) {
+        var verse = "";
+        // Zufallsindex für Subjekt
+        var subjectIndex = Math.floor(Math.random() * _subjects.length);
+        verse += _subjects.splice(subjectIndex, 1)[0] + " "; // Subjekt hinzufügen und aus dem Array entfernen
+        // Zufallsindex für Prädikat
+        var predicateIndex = Math.floor(Math.random() * _predicates.length);
+        verse += _predicates.splice(predicateIndex, 1)[0] + " "; // Prädikat hinzufügen und entfernen
+        // Zufallsindex für Objekt
+        var objectIndex = Math.floor(Math.random() * _objects.length);
+        verse += _objects.splice(objectIndex, 1)[0]; // Objekt hinzufügen und entfernen
+        return verse; // Generierten Vers zurückgeben
+    }
+    // Ausgabe der Verse in der Konsole
+    for (var i = subjects.length; i > 0; i--) {
+        var line = getVerse(subjects, predicates, objects);
+        console.log(line); // Gedichtzeile ausgeben
+    }
+})(RandomPoem2 || (RandomPoem2={}));
+let RandomPoem2 = "This is a random poem.";
--- Übungsaufgaben/Übung 1/gedicht.ts
+++ Übungsaufgaben/Übung 1/gedicht.ts
@@ -1,1 +1,31 @@
-bla
+namespace RandomPoem2 {
+    // Arrays für Subjekte, Prädikate und Objekte
+    const subjects: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    const predicates: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    const objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+    // Funktion zur Generierung eines Verses
+    function getVerse(_subjects: string[], _predicates: string[], _objects: string[]): string {
+        let verse: string = "";
+
+        // Zufallsindex für Subjekt
+        const subjectIndex = Math.floor(Math.random() * _subjects.length);
+        verse += _subjects.splice(subjectIndex, 1)[0] + " "; // Subjekt hinzufügen und aus dem Array entfernen
+
+        // Zufallsindex für Prädikat
+        const predicateIndex = Math.floor(Math.random() * _predicates.length);
+        verse += _predicates.splice(predicateIndex, 1)[0] + " "; // Prädikat hinzufügen und entfernen
+
+        // Zufallsindex für Objekt
+        const objectIndex = Math.floor(Math.random() * _objects.length);
+        verse += _objects.splice(objectIndex, 1)[0]; // Objekt hinzufügen und entfernen
+
+        return verse; // Generierten Vers zurückgeben
+    }
+
+    // Ausgabe der Verse in der Konsole
+    for (let i = subjects.length; i > 0; i--) {
+        const line = getVerse(subjects, predicates, objects);
+        console.log(line); // Gedichtzeile ausgeben
+    }
+}
